e756b259101f39b9efb5bb2c9380094fb5e1d4dd
Tanish Azad <tanishazad03@gmail.com>
2024-04-03 10:01:24 +0530

    init

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "io.github"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+    implementation("com.jcabi:jcabi-github:1.8.0")
+
+	implementation("org.springframework.boot:spring-boot-starter")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/io/github/taz03/Taz03ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.taz03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taz03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
